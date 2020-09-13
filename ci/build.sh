aa9971bf5f3136e952feecaa7be3a26869076739
antelle <dmitryvit@gmail.com>
2020-09-13 17:27:48 +0200

    fixing build

--- ci/build.sh
+++ ci/build.sh
@@ -3,11 +3,10 @@
 set -e
 set -o pipefail
 
-docker exec -it emsdk update
-docker exec -it emsdk install latest
-docker exec -it emsdk activate latest
+docker exec -it emscripten emsdk update && emsdk install latest && emsdk activate latest
 
 docker exec -it emscripten ./build.sh
+
 npm test
 npm run prepare-deployment -- --commit="$TRAVIS_COMMIT" --tag="$TRAVIS_TAG" --branch="$TRAVIS_BRANCH"
 npm pack
